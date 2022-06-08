acb5b880b3a95cde1c16642e8918b01f32dd448a
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-06-08 17:06:53 +0100

    added HTML selectors in JS

---
+++ main.js
@@ -0,0 +1,8 @@
+// Selectors from HTML
+
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let add = document.getElementById("add");
