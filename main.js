61e1330e2818b8cdc492603268c46883cf6c8519
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-06-09 16:50:17 +0100

    fixed the file name to match the path, added warning message color for a blank task, added fom validation in JS

--- main.js
+++
@@ -1,8 +0,0 @@
-// Selectors from HTML
-
-let form = document.getElementById("form");
-let textInput = document.getElementById("textInput");
-let dateInput = document.getElementById("dateInput");
-let textarea = document.getElementById("textarea");
-let msg = document.getElementById("msg");
-let add = document.getElementById("add");
---
+++ script.js
@@ -0,0 +1,26 @@
+// Selectors from HTML
+
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+// Form validation
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Task cannot be blank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+  }
+};
