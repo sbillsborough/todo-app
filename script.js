7e10bcce5818e0d80176e1f24c2a087d538e33c1
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-06-20 09:23:05 +0100

    edit task function added

--- script.js
+++ script.js
@@ -71,14 +71,6 @@ let createTasks = () => {
   resetForm();
 };
 
-// Reset form
-
-let resetForm = () => {
-  textInput.value = "";
-  dateInput.value = "";
-  textarea.value = "";
-};
-
 //  Delete task
 
 let deleteTask = (e) => {
@@ -86,4 +78,12 @@ let deleteTask = (e) => {
   data.splice(e.parentElement.parentElement.id, 1);
   localStorage.setItem("data", JSON.stringify(data));
   console.log(data);
+};
+
+// Reset form
+
+let resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
 };
