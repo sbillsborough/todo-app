9959b31e979d3d4cf40818862d38d431ef619b0e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-06-20 09:15:35 +0100

    delete task function added

--- script.js
+++ script.js
@@ -78,3 +78,12 @@ let resetForm = () => {
   dateInput.value = "";
   textarea.value = "";
 };
+
+//  Delete task
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+};
